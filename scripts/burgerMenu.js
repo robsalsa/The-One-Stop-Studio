bec2dc400f568e8897b14aedb5dbfe4fd23b8ad2
Robert <robertosalazarvasquezalexis@gmail.com>
2025-08-12 09:20:55 -0700

    Added a Close Button for the Burger Menu when using smaller screens

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -1,7 +1,13 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
+const closeButton=document.querySelector('.close-button');
 
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   burger.classList.toggle('open');
+});
+
+closeButton.addEventListener('click', () => {
+  navLinks.classList.remove('open');
+  burger.classList.remove('open');
 });
