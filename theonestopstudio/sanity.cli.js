75507a559287eaa3ef3373a6dc40c9e5dac8c2d2
Robert <robertosalazarvasquezalexis@gmail.com>
2025-11-05 12:30:04 -0800

    Another Massive Correction of CMS

--- theonestopstudio/sanity.cli.js
+++ theonestopstudio/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'j2hhua6h',
+    projectId: 'goyzu79a',
     dataset: 'production'
   },
   /**
--- theonestopstudio/sanity.config.js
+++ theonestopstudio/sanity.config.js
@@ -5,9 +5,9 @@ import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'oneStopShop',
+  title: 'theonestopstudio',
 
-  projectId: 'j2hhua6h',
+  projectId: 'goyzu79a',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
