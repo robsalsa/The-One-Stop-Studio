766f5eb3f75bf7277ff94d0c31aa1a1e20af678f
Robert <robertosalazarvasquezalexis@gmail.com>
2025-11-29 11:35:35 -0800

    update name

--- theonestopstudio-app/src/app/[lang]/service/page.tsx
+++ theonestopstudio-app/src/app/[lang]/service/page.tsx
@@ -3,6 +3,13 @@
 import React, { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import translations from '@/../public/localization/translationServicePage.json';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: "Service Page - The One Stop Studio",
+  description: "All Services and Filters for said services"
+}
+
 
 type Service = {
   name: string;
