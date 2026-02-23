4cc8f1d721b1ad207b31533761fcb7a61e911d1a
Robert <robertosalazarvasquezalexis@gmail.com>
2026-02-22 21:51:49 -0800

    hotfix

--- theonestopstudio-app/src/app/[lang]/booking/page.tsx
+++ theonestopstudio-app/src/app/[lang]/booking/page.tsx
@@ -1,24 +1,20 @@
-'use client';
 
+  
 import Cal, { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
-
-export default function BookingCALPage() {
+export default function MyApp() {
   useEffect(() => {
     (async function () {
-      const cal = await getCalApi({ namespace: "booking" });
-      cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+      const cal = await getCalApi({"namespace":"booking"});
+      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
     })();
-  }, []);
-
-  return (
-    <div className="w-full h-screen min-h-screen">
-      <Cal
-        namespace="test-booking"
-        calLink="the-one-stop-shop-wendy/test-booking"
-        style={{ width: "100%", height: "100%", overflow: "scroll", marginTop: "150px" }}
-        config={{ layout: "month_view", useSlotsViewOnSmallScreen: "true" }}
-      />
-    </div>
-  );
-}
+  }, [])
+  return <Cal namespace="booking"
+    calLink="the-one-stop-shop-wendy/booking"
+    style={{width:"100%",height:"100%",overflow:"scroll"}}
+    config={{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}}
+    
+    
+  />;
+};
+  
